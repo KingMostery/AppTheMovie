8b37332978049ca28c69eefbdefbe92807e10cb6
LUIS ANDRES DURANGO RONDON <luis.durango.575@unisabaneta.edu.co>
2022-08-26 21:49:28 -0500

    Creacion de la Aplicación

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+
+export default function App() {
+  console.log('Hola como etas');
+  return (
+    <SafeAreaView>
+      <Text>App mejorado</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const API_KEY = '696941f083f32aed0bdb68812b108ad7';
